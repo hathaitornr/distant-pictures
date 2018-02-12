fcc79a5c5976c4f0898605c3e61ffe2f2f61ce87
hathaitornr <hathaitornr@gmail.com>
2018-02-11 21:23:51 -0500

    Implemented GIF function :)

--- pictureServer.js
+++ pictureServer.js
@@ -4,7 +4,7 @@ server.js
 Authors:David Goedicke (da.goedicke@gmail.com) & Nikolas Martelaro (nmartelaro@gmail.com)
 
 This code is heavily based on Nikolas Martelaroes interaction-engine code (hence his authorship).
-The  original purpose was:
+The    original purpose was:
 This is the server that runs the web application and the serial
 communication with the micro controller. Messaging to the micro controller is done
 using serial. Messaging to the webapp is done using WebSocket.
@@ -36,13 +36,13 @@ app.use(express.static('public')); // find pages in public directory
 // check to make sure that the user provides the serial port for the Arduino
 // when running the server
 if (!process.argv[2]) {
-  console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
-  process.exit(1);
+    console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
+    process.exit(1);
 }
 
 // start the server and say what port it is on
 http.listen(serverPort, function () {
-  console.log('listening on *:%s', serverPort);
+    console.log('listening on *:%s', serverPort);
 });
 //----------------------------------------------------------------------------//
 
@@ -50,26 +50,26 @@ http.listen(serverPort, function () {
 //----------------------------WEBCAM SETUP------------------------------------//
 //Default options
 var opts = { //These Options define how the webcam is operated.
-  //Picture related
-  width: 1280, //size
-  height: 720,
-  quality: 100,
-  //Delay to take shot
-  delay: 0,
-  //Save shots in memory
-  saveShots: true,
-  // [jpeg, png] support varies
-  // Webcam.OutputTypes
-  output: "jpeg",
-  //Which camera to use
-  //Use Webcam.list() for results
-  //false for default device
-  device: false,
-  // [location, buffer, base64]
-  // Webcam.CallbackReturnTypes
-  callbackReturn: "location",
-  //Logging
-  verbose: false
+    //Picture related
+    width: 1280, //size
+    height: 720,
+    quality: 100,
+    //Delay to take shot
+    delay: 0,
+    //Save shots in memory
+    saveShots: true,
+    // [jpeg, png] support varies
+    // Webcam.OutputTypes
+    output: "jpeg",
+    //Which camera to use
+    //Use Webcam.list() for results
+    //false for default device
+    device: false,
+    // [location, buffer, base64]
+    // Webcam.CallbackReturnTypes
+    callbackReturn: "location",
+    //Logging
+    verbose: false
 };
 var Webcam = NodeWebcam.create(opts); //starting up the webcam
 //----------------------------------------------------------------------------//
@@ -80,60 +80,86 @@ var Webcam = NodeWebcam.create(opts); //starting up the webcam
 // start the serial port connection and read on newlines
 const serial = new SerialPort(process.argv[2], {});
 const parser = new Readline({
-  delimiter: '\r\n'
+    delimiter: '\r\n'
 });
 
 // Read data that is available on the serial port and send it to the websocket
 serial.pipe(parser);
 parser.on('data', function (data) {
-  console.log('Data:', data);
-  console.log('Helllllo');
-  takePicture();
-  io.emit('server-msg', data);
+    console.log('Data:', data);
+    console.log('Helllllo');
+    takePicture();
+    io.emit('server-msg', data);
 });
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+var imageNames = [];
 //----------------------------------------------------------------------------//
 
 function takePicture() {
 
-  /// First, we create a name for the new picture.
-  /// The .replace() function removes all special characters from the date.
-  /// This way we can use it as the filename.
-  var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+    /// First, we create a name for the new picture.
+    /// The .replace() function removes all special characters from the date.
+    /// This way we can use it as the filename.
+    var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
 
-  console.log('making a making a picture at' + imageName); // Second, the name is logged to the console.
+    console.log('Capturing a picture at ' + imageName + '.jpg'); // Second, the name is logged to the console.
 
-  //Third, the picture is  taken and saved to the `public/`` folder
-  NodeWebcam.capture('public/' + imageName, opts, function (err, data) {
-    io.emit('newPicture', (imageName + '.jpg')); ///Lastly, the new name is send to the client web browser.
-    /// The browser will take this new name and load the picture from the public folder.
-  });
+    //Third, the picture is    taken and saved to the `public/`` folder
+    NodeWebcam.capture('public/' + imageName, opts, function (err, data) {
+        io.emit('newPicture', (imageName + '.jpg')); 
+    });
+    
+    // Append the image name to the list.
+    imageNames.push(imageName + '.jpg');
+}
 
+function transmitImageNames() {
+    io.emit('newGif', imageNames);
 }
 
 //---------------------- WEBSOCKET COMMUNICATION (web browser)----------------//
 // this is the websocket event handler and say if someone connects
 // as long as someone is connected, listen for messages
 io.on('connect', function (socket) {
-  console.log('a user connected');
+    // Timer responsible for taking pictures with the camera.
+    var cameraTimer;
 
-  // if you get the 'ledON' msg, send an 'H' to the Arduino
-  socket.on('ledON', function () {
-    console.log('ledON');
-    serial.write('H');
-  });
+    console.log('a user connected');
 
-  // if you get the 'ledOFF' msg, send an 'L' to the Arduino
-  socket.on('ledOFF', function () {
-    console.log('ledOFF');
-    serial.write('L');
-  });
+    // if you get the 'ledON' msg, send an 'H' to the Arduino
+    socket.on('ledON', function () {
+        console.log('ledON');
+        serial.write('H');
+    });
 
-  //-- Addition: This function is called when the client clicks on the `Take a picture` button.
-  socket.on('takePicture', takePicture);
-  // if you get the 'disconnect' message, say the user disconnected
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
+    // if you get the 'ledOFF' msg, send an 'L' to the Arduino
+    socket.on('ledOFF', function () {
+        console.log('ledOFF');
+        serial.write('L');
+    });
+
+    //-- Addition: This function is called when the client clicks on the `Take a picture` button.
+    socket.on('takePicture', takePicture);
+    // if you get the 'disconnect' message, say the user disconnected
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    
+    // Start recording images at a fixed interval.
+    socket.on('startRecording', function() {
+        console.log('[SERVER] Recording...');
+        imageNames = [];
+        cameraTimer = setInterval(takePicture, 1000);
+    });
+
+    // Stop recording images.
+    socket.on('stopRecording', function() {
+        console.log('[SERVER] Stopped recording.');
+        clearInterval(cameraTimer);
+        transmitImageNames();
+    });
 
 });
-//----------------------------------------------------------------------------//
+
